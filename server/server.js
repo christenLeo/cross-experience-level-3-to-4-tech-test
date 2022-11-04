a9d2c01d01a737d6631e8ce41bebc1daabf39a4c
Leona Christen Macedo <leo.christenmacedo@gmail.com>
2022-11-04 14:55:19 -0300

    [task]: 03 endpoints / POST Validations

--- server/server.js
+++ server/server.js
@@ -23,10 +23,57 @@ app.prepare().then(() => {
         try {
             const {cardNum, month, year, cvv, ownerName} = req.body;
 
+            // Checking if the fields are filled
             if (!cardNum || !month || !year || !cvv || !ownerName) {
                 statusCode = 400;
                 throw new Error('Please check your infos, all fields must be filled');
             }
+
+            // Checking if the card have 16 digits or 15 case the card is an American Express
+            const cardOnlyNums = cardNum.split('').filter((char) => {return char !== " "});
+
+            if (cardOnlyNums.length < 15 || cardOnlyNums.length > 16) {
+                statusCode = 400;
+                throw new Error('Please check your card number');
+            } 
+
+            // Checking if all the digits are numbers
+            for (let i = 0; i < cardOnlyNums.length; i++) {
+                let num = Number(cardOnlyNums[i]);
+
+                if (String(num) === 'NaN') {
+                    statusCode = 400;
+                    throw new Error('Please check your card number');
+                }
+            }
+
+            // Check if it has balance??
+
+            // Check the month 
+            if (month < 1 || month > 12) {
+                statusCode = 400;
+                throw new Error('Please check the month number');
+            }
+
+            // Check the year
+
+            // Check the cvv
+            const cvvArr = cvv.split('');
+
+            if (cvvArr.length < 3 || cvvArr.length > 4) {
+                statusCode = 400;
+                throw new Error('Please check your cvv number');
+            }
+
+            for (let i = 0; i < cvvArr.length; i++) {
+                let num = Number(cvvArr[i]);
+
+                if (String(num) === 'NaN') {
+                    statusCode = 400;
+                    throw new Error('Please check your cvv number');
+                }
+            }
+
 
             const id = idGenerator();
             const newPlanSign = {
@@ -40,12 +87,12 @@ app.prepare().then(() => {
 
             res.status(statusCode).send({message: 'Plan signed successfully', info: newPlanSign});
         } catch (error) {
-            if (statusCode === 400) {
-                res.status(statusCode).send({message: error.message});
-            }
             if (statusCode === 200) {
                 statusCode = 500;
                 res.status(statusCode).send({message: 'Internal server error, please try again'})
+            }
+            else {
+                res.status(statusCode).send({message: error.message});
             }
         }
     });
